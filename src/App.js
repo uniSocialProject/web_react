63c07af2124e52f157fa861d4993220e4aefef93
MuratOzte <mo2553481@gmail.com>
2023-08-30 20:01:06 +0300

    mui & router added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-
 function App() {
-  return (
-    <div>
-      <h1>hi</h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1>hi</h1>
+        </div>
+    );
 }
 
 export default App;
