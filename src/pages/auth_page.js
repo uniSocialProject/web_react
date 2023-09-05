7401179c5ea58e1416cd34ae9d6b25596f90eed0
MuratOzte <mo2553481@gmail.com>
2023-09-06 01:40:08 +0300

    New logo & mui/icons added

--- src/pages/auth_page.js
+++ src/pages/auth_page.js
@@ -1,84 +1,77 @@
-import {
-  Container,
-  Typography,
-  Box,
-  TextField,
-  Grid,
-  Button,
-} from "@mui/material";
-import image from "../images/unisocial.png";
+import { Typography, Box, TextField, Grid, Button } from '@mui/material';
+import image from '../images/logo.png';
 function AuthPage() {
-  return (
-    <Box
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      minHeight="100vh"
-      sx={{ backgroundColor: "#F7F7F7" }}
-    >
-      <Grid container>
-        <Grid
-          item
-          xs={false}
-          sm={4}
-          md={7}
-          sx={{
-            backgroundImage: `url(${image})`,
-            backgroundRepeat: "no-repeat",
+    return (
+        <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            minHeight="100vh"
+            sx={{ backgroundColor: '#F7F7F7' }}
+        >
+            <Grid container>
+                <Grid
+                    item
+                    xs={false}
+                    sm={4}
+                    md={7}
+                    sx={{
+                        backgroundImage: `url(${image})`,
+                        backgroundRepeat: 'no-repeat',
 
-            backgroundSize: "contain",
-            backgroundPosition: "center",
-          }}
-        />
-        <Grid item xs={12} sm={8} md={5}>
-          <Box
-            sx={{
-              my: 8,
-              mx: 4,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <Typography component="h1" variant="h5">
-              Giriş Yap
-            </Typography>
-            <Box component="form" sx={{ mt: 1 }}>
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email"
-                name="email"
-                autoComplete="email"
-                autoFocus
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Şifre"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
+                        backgroundSize: 'contain',
+                        backgroundPosition: 'center',
+                    }}
+                />
+                <Grid item xs={12} sm={8} md={5}>
+                    <Box
+                        sx={{
+                            my: 8,
+                            mx: 4,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <Typography component="h1" variant="h5">
+                            Giriş Yap
+                        </Typography>
+                        <Box component="form" sx={{ mt: 1 }}>
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email"
+                                name="email"
+                                autoComplete="email"
+                                autoFocus
+                            />
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Şifre"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password"
+                            />
 
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Giriş Yap
-              </Button>
-            </Box>
-          </Box>
-        </Grid>
-      </Grid>
-    </Box>
-  );
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={{ mt: 3, mb: 2 }}
+                            >
+                                Giriş Yap
+                            </Button>
+                        </Box>
+                    </Box>
+                </Grid>
+            </Grid>
+        </Box>
+    );
 }
 
 export default AuthPage;
